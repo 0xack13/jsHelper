d7fcc6db308b98ccfdecb2ddbf3d70e52358b2f5
0xack13 <0xack13@gmail.com>
2014-12-25 11:04:33 +0300

    reveal module pattern

--- std.js
+++ std.js
@@ -1,8 +1,15 @@
-function toCamelCase(sentenceCase) {
-    var out = "";
-    sentenceCase.split(" ").forEach(function (el, idx) {
-        var add = el.toLowerCase();
-        out += (idx === 0 ? add : add[0].toUpperCase() + add.slice(1));
-    });
-    return out;
-}
+
+//Basic usage: std.toCamelCase()
+std = (function () {
+	var pub = {};
+	pub.toCamelCase = function () {
+		var sentenceCase = "WOW",
+			out = "";
+	    sentenceCase.split(" ").forEach(function (el, idx) {
+	        var add = el.toLowerCase();
+	        out += (idx === 0 ? add : add[0].toUpperCase() + add.slice(1));
+	    });
+	    return out;
+	};
+    return pub;
+})();
